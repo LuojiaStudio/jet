b409313e9638d3b2a19b940f3de31385437a86de
Jsceoz <jsceoz96@gmail.com>
2017-02-06 18:55:42 +0800

    flush

--- src/App.js
+++ src/App.js
@@ -5,25 +5,13 @@ import './App.css';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import { Router, Route, hashHistory } from 'react-router'
 import injectTapEventPlugin from 'react-tap-event-plugin';
-import NewsList from './components/NewsList'
-import NewsCard from './components/NewsCard'
-import NewsDetail from './components/NewsDetail'
-import TopBar from './components/TopBar'
-import Slide from './components/Slide'
-
 injectTapEventPlugin();
 
 class App extends Component {
     render() {
         return (
             <MuiThemeProvider>
-                <div>
-                    <TopBar/>
-                    <Router history={hashHistory}>
-                        <Route path="/" component={NewsList}/>
-                        <Route path="/news" component={NewsDetail}/>
-                    </Router>
-                </div>
+
             </MuiThemeProvider>
         );
     }
